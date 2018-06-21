08c948a2e7e1070e28a9f24153762af94585de7a
fandanping <18602474229@163.com>
2018-06-21 11:49:54 +0800

    change

--- vue-cli-count/src/components/total.vue
+++ vue-cli-count/src/components/total.vue
@@ -3,41 +3,119 @@
   购物车清单
    <ul>
      <li>
-       <span>全选</span><span>商品</span><span>数量</span><span>单价</span><span>金额</span><span>操作</span>
+       <span>全选<input type="checkbox" @click="selectAll()" v-model="checked"></span><span>商品</span><span>数量</span><span>单价</span><span>金额</span><span>操作</span>
+       <label>
+        全选 {{checked}}
+       </label>
      </li>
-     <li v-for="item in items">
-       <span><input type="checkbox"></span><span>{{item.name}}</span><span><button>-1</button>1<button>+1</button></span><span>{{item.price}}</span><span>jkj</span><span>删除</span>
+     <li v-for="(item,index) in items">
+       <span><input type="checkbox" v-model="checkedNames" :value="item.name" @click="itemCheck(index)"></span><span>{{item.name}}</span><span><button @click="decCount(index)">-1</button>{{item.count}}<button @click="addCount(index)">+1</button></span><span>{{item.price}}</span><span>{{item.price * item.count}}</span><span @click="deleteItem(index)">删除</span>
      </li>
    </ul>
+  <div><span>共{{getTotal.totalNum}}件商品</span><span>总价：{{getTotal.totalPrice}}</span></div>
 </div>
 </template>
 <script>
   export default{
     data(){
       return{
+        checked: false,
+        checkedNames: [],
+        checkedArr: ["Web Development", "Design", "Integration","Training"],
         items: [
           {
             name: 'Web Development',
             price: 300,
-            active:false
+            active:false,
+            count:1,
+            select:false
           },{
             name: 'Design',
             price: 400,
-            active:false
+            active:false,
+            count:1,
+            select:false
           },{
             name: 'Integration',
             price: 250,
-            active:false
+            active:false,
+            count:1,
+            select:false
           },{
             name: 'Training',
             price: 220,
-            active:false
+            active:false,
+            count:1,
+            select:false
           }
         ]
       }
     },
-    methods:{
+    computed:{
+      getTotal:function(){
+           var proList=this.items.filter(function(val){
+             return val.select ==true;
+           })
+           var totalPrice=0;
 
+           for(var i=0,len=proList.length;i<len;i++){
+
+             totalPrice +=proList[i].price *proList[i].count;
+   /*          console.log(proList[i].price);
+             console.log(proList[i].count);
+             console.log(totalPrice);*/
+           }
+           return {
+              totalNum:proList.length,totalPrice:totalPrice
+           }
+      }
+    },
+    methods:{
+      //全选
+      selectAll:function(){
+        if (this.checked) {
+          this.checkedNames = []
+        } else {
+          this.checkedNames = this.checkedArr
+        }
+      },
+      addCount:function(index){
+        this.items[index].count++;
+
+      },
+      decCount:function(index){
+        if(this.items[index].count>0){
+          this.items[index].count--;
+        }else{
+          alert("你不能在减少了");
+        }
+      },
+      deleteItem:function(index){
+        this.items.splice(index,1);
+      },
+      itemCheck:function(index){
+            if(this.items[index].select){
+              this.items[index].select=false;
+            }else{
+              this.items[index].select=true;
+            }
+      }
+    },
+    watch: {
+      "checkedNames": function() {
+        if (this.checkedNames.length == this.checkedArr.length) {
+          this.checked = true
+        } else {
+          this.checked = false
+        }
+      }
+    },
+    mounted: function () {
+   /*   var _this=this;
+      //为productList添加select（是否选中）字段，初始值为true
+      this.items.map(function (item) {
+        _this.$set(item, 'select', true);
+      })*/
     }
   }
 </script>
